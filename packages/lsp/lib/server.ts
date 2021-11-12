56aba3afab43bd9bfa99998a814b7503c2fa5668
Damian Arquez <damian.arquez.t@gmail.com>
2021-11-12 12:39:56 -0300

    Fix language server

--- packages/lsp/lib/server.ts
+++ packages/lsp/lib/server.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import {
   createConnection,
   TextDocuments,
@@ -14,6 +16,8 @@ import { parse } from '@gsens-lang/parsing';
 
 const connection = createConnection(ProposedFeatures.all);
 
+connection.console.log('running');
+
 const documents = new TextDocuments(TextDocument);
 
 connection.onInitialize(() => {
@@ -26,32 +30,47 @@ connection.onInitialize(() => {
   };
 });
 
+const parseFile = (doc: TextDocument): void => {
+  try {
+    const { failures } = parse(doc.getText());
+
+    const diagnostics: Diagnostic[] = [];
+
+    failures.map(({ reason, token }) => {
+      const line = token.line ?? 0;
+
+      const diagnostic = Diagnostic.create(
+        { start: { line, character: 0 }, end: { line, character: 1024 } },
+        reason ?? 'Syntax error',
+        DiagnosticSeverity.Error,
+      );
+
+      diagnostics.push(diagnostic);
+    });
+
+    connection.sendDiagnostics({
+      uri: doc.uri,
+      diagnostics,
+    });
+  } catch (e) {
+    connection.console.error(e);
+  }
+};
+
 documents.onDidOpen((event) => {
-  const { failures } = parse(event.document.getText());
+  connection.console.info('Open file');
+  parseFile(event.document);
+});
 
-  const diagnostics: Diagnostic[] = [];
-
-  failures.map(({ reason, token }) => {
-    const line = token.line ?? 0;
-
-    const diagnostic = Diagnostic.create(
-      { start: { line, character: 0 }, end: { line, character: 1024 } },
-      reason ?? 'Syntax error',
-      DiagnosticSeverity.Error,
-    );
-
-    diagnostics.push(diagnostic);
-  });
-
-  connection.sendDiagnostics({
-    uri: event.document.uri,
-    diagnostics,
-  });
+documents.onDidChangeContent((event) => {
+  connection.console.info('Change file');
+  parseFile(event.document);
 });
 
 const keywords = ['fun', 'var', 'print']; // TODO: Export them from parsing module
 
 connection.onCompletion(() => {
+  connection.console.info('completion');
   const keywordCompletions: CompletionItem[] = keywords.map((kw) => ({
     label: kw,
     kind: CompletionItemKind.Keyword,
@@ -59,3 +78,6 @@ connection.onCompletion(() => {
 
   return keywordCompletions;
 });
+
+documents.listen(connection);
+connection.listen();
