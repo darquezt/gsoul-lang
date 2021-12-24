bdf3c92f25d71ecd2d1d97e196f8a71e7fc74849
Damian Arquez <damian.arquez.t@gmail.com>
2021-12-24 18:50:34 -0300

    Fix chalk import issues

--- packages/runtime/utils/format.ts
+++ packages/runtime/utils/format.ts
@@ -1,15 +1,15 @@
-import chalk from 'chalk';
+import { cyan } from 'chalk';
 import { SimpleValue } from '../elaboration/ast';
 
 export const formatValue = (value: SimpleValue): string => {
   switch (value.kind) {
     case 'RealLiteral':
-      return chalk.cyan(value.value);
+      return cyan(value.value);
     case 'BoolLiteral':
-      return chalk.cyan(String(value.value));
+      return cyan(String(value.value));
     case 'NilLiteral':
-      return chalk.cyan('nil');
+      return cyan('nil');
     case 'Closure':
-      return chalk.cyan('[Closure]');
+      return cyan('[Closure]');
   }
 };
